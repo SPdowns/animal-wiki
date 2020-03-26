86b76e55046414a5ac97464be52cced8fb91aa48
shonburgundy <seanbeloussow@yahoo.com>
2020-03-26 11:37:59 -0700

    updated html, css, js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
   $(".clickable-turtle").click(function() {
+    $("#bear-showing").fadeOut();
+    $("#lion-showing").fadeOut();
     $("#turtle-showing").fadeToggle(); 
   });
   $(".clickable-lion").click(function() {
+    $("#bear-showing").fadeOut();
+    $("#turtle-showing").fadeOut();
     $("#lion-showing").fadeToggle(); 
+  });
+  $(".clickable-bear").click(function() {
+    $("#turtle-showing").fadeOut();
+    $("#lion-showing").fadeOut();
+    $("#bear-showing").fadeToggle();
   });
 });
 
+  //var animal =(prompt("Which animal would you like to learn about? Turtles, Lions or Bears?"));
+
+  if (animal === "turtle") {
+    $('#turtle-showing').show();
+  }
